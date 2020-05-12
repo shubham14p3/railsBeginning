72a2f6549108a3193a40b425f95581d4c9a3220f
shubham14p3 <shubham14p3@gmail.com>
2020-05-12 22:38:07 +0530

    Adding Files

---
+++ db/migrate/20200512170610_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
